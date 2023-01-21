fcef2a4df98fe4f8d66ee490cb03d44e9f2546bf
jisoo <dlwltn0350@naver.com>
2023-01-21 23:00:20 +0900

    ⭐ FEAT : spring init setting

---
+++ backend/learnway/src/main/java/com/ssafy/learnway/LearnwayApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.learnway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnwayApplication.class, args);
+	}
+
+}
---
+++ backend/learnway/src/test/java/com/ssafy/learnway/LearnwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.learnway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
