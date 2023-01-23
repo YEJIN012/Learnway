ed75d725cc90e3a0ecfae5e7d16f27d45876439a
mydream11123 <mydream11123@naver.com>
2023-01-23 21:52:08 +0900

    ⭐ FEAT : react initial setting

---
+++ frontend/learnway/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Styled from 'styled-components'
+
+const Label = Styled.span`
+  font-size:10px;
+`
+function App() {
+  return (
+    <Label>Hello World</Label>
+  );
+}
+
+export default App;
