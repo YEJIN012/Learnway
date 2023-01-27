be29477f15adb0289b6a73779bfff95e092b230b
chohm1223 <chohm1223@naver.com>
2023-01-27 10:15:50 +0900

    🛠fix: #3Transactional 추가 및 출력 제거

--- backend/learnway/src/main/java/com/ssafy/learnway/service/FriendService.java
+++ backend/learnway/src/main/java/com/ssafy/learnway/service/FriendService.java
@@ -5,6 +5,7 @@ import com.ssafy.learnway.domain.user.User;
 import com.ssafy.learnway.repository.FriendRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +16,7 @@ public class FriendService {
     @Autowired
     FriendRepository friendRepository;
 
+    @Transactional(readOnly = true)
     public List<Friend> list(User user) {
         // 친구 관계를 한 번만 저장하기 때문에 두 번 탐색해야 함
         List<Friend> friendListByUID = friendRepository.findAllByUserId(user);
@@ -22,29 +24,28 @@ public class FriendService {
         List<Friend> friendList = new ArrayList<>();
 
         if (friendListByUID.isEmpty() && friendListByFID.isEmpty()) {
-            System.out.println("1111111111111111");
             return Stream.concat(friendListByUID.stream(), friendListByFID.stream()).toList();
         } else if (friendListByUID.isEmpty() && !friendListByFID.isEmpty()) {
-            System.out.println("2222222222222222");
             return friendListByFID;
 
         } else if (!friendListByUID.isEmpty() && friendListByFID.isEmpty()) {
-            System.out.println("33333333333333333");
             return friendListByUID;
         }
         return friendList;
     }
 
+    @Transactional
     public void make(Friend newRelation) {
         friendRepository.save(newRelation);
     }
 
+    @Transactional
     public void delete(Friend relation) {
         friendRepository.delete(relation);
     }
 
+    @Transactional(readOnly = true)
     public Friend findById(User userId, User friendId) {
-
         return friendRepository.findByUserIdAndFriendId(userId, friendId);
     }
 }
