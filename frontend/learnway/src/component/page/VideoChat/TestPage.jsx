5d9b3b57b2cd56fa1c2dfeb83ef08ad156ff92f2
차영후 <mydream11123@naver.com>
2023-01-31 01:06:28 +0900

    ⭐ FEAT : #14 Add additonal service page

---
+++ frontend/learnway/src/component/page/VideoChat/TestPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+import Translate from './Translate';
+
+function Translate(){
+    return(
+        <Translate></Translate>
+    );
+};
+export default Translate;
