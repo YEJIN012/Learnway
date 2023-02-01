8fedebd83fa590caa89554ea789b8b7d5ee17fa6
mydream11123 <mydream11123@naver.com>
2023-02-01 13:19:28 +0900

    📝 UPDATE : #14 Report, Quit Component

--- frontend/learnway/src/component/VideoChat/Quit/Quit.jsx
+++ frontend/learnway/src/component/VideoChat/Quit/Quit.jsx
@@ -1,6 +1,7 @@
+import { TabUnselectedOutlined } from "@mui/icons-material";
 import React from "react";
 import styled from "styled-components";
-import TFBtnSet from "./TFBtnSet";
+import TFBtnSet from "../CommonComponent/TFBtnSet";
 
 const Frame = styled.div`
     display:flex;
@@ -14,13 +15,19 @@ const Msg = styled.span`
     font-family : system-ui;
     font-size:2vw;
 `;
-
+function quit(){
+    alert("나가기 처리");
+}
+function cancel(){
+    alert("나가기 취소 처리");
+}
 
 function Leave() {
+    
     return (
         <Frame>
         <Msg>Do  you want to leave random matching?</Msg>
-        <TFBtnSet radius="5px" width="5vw" height="2vw" fontSize="1vw"></TFBtnSet>
+        <TFBtnSet function_ok={()=>{quit()}} function_cancel={()=>{cancel()}} radius="5px" width="5vw" height="2vw" fontSize="1vw"></TFBtnSet>
         </Frame>
     );
 
