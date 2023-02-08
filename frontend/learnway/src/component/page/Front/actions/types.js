9dcfda010e25e093fb98d5b5bfdb61cafea4e38e
Lee-hanbin <hanbin0501@gmail.com>
2023-02-08 17:07:07 +0900

    ⭐ FEAT : #37 access token 만료시 재발급 기능 구현

--- frontend/learnway/src/component/page/Front/actions/types.js
+++ frontend/learnway/src/component/page/Front/actions/types.js
@@ -3,12 +3,13 @@ export const GET_INTEREST = "GET_INTEREST";
 export const GET_LANGUAGE = "GET_LANGUAGE";
 
 export const LOGIN_USER = "LOGIN_USER";
+export const LOGOUT_USER = "LOGOUT_USER";
+
 export const SET_TOKEN = "SET_TOKEN";
+export const GET_TOKEN = "GET_TOKEN"
+export const DELETE_TOKEN = "DELETE_TOKEN";
 
 export const UPDATE_USER = "UPDATE_USER";
-
-export const LOGOUT_USER = "LOGOUT_USER";
-export const DELETE_TOKEN = "DELETE_TOKEN";
 
 export const DELETE_INFO = "DELETE_INFO";
 
---
+++ frontend/learnway/src/component/page/Front/utils/CheckToken.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react"
+import { getCookieToken, setRefreshToken } from "./Cookie";
+import { useDispatch, useSelector } from 'react-redux';
+import { DELETE_TOKEN } from "../actions/types";
+import { accessToken, requestToken } from "../actions/userAction";
+
+
+export function CheckToken(key) {
+  const [isAuth, SetIsAuth] = useState('Loaded');
+  const tokenData = useSelector(state => state.TokenReducer);
+  const refreshToken = getCookieToken();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const checkAuthToken = async () => {
+      // refreshToken이 undefined이면 accessToken 삭제
+      if (refreshToken === undefined) {
+        dispatch({type: DELETE_TOKEN})
+        SetIsAuth('Failed');
+      } else {// refreshToken이 존재
+        // access 토큰이 존재하고, 기간이 만료되지 않았으면 Success 
+        if ( tokenData.authenticated && new Date().getTime() < tokenData.expireTime){
+          SetIsAuth('Success');
+        } else { // 그렇지 않으면
+          // refreshToken을 통해 토큰 재발급
+          const addToken = {accessToken: tokenData.accessToken ,refreshToken: refreshToken}
+          const response = await requestToken(addToken);
+          response.payload
+            .then((res) => {
+              const status = res.status
+              const msg = res.msg
+              console.log(msg)
+              // 토큰 발급이 성공하면 토큰 갱신
+              if (status === 200) {
+                setRefreshToken(res.token.refreshToken);
+                const getaccessToken = accessToken(res.token);
+                dispatch({type: getaccessToken.type, payload: getaccessToken.payload});
+              }
+            })
+        }
+      }
+    };
+    checkAuthToken();
+  }, [ refreshToken, dispatch, key ]);
+  return(
+    isAuth
+  )
+}
