08e01a7d3da2a37bfa19943962fa4b69d571fe8b
jisoo <dlwltn0350@naver.com>
2023-02-12 03:22:30 +0900

    🎨 DESIGN : #4 언어 선택 부분 처리

--- frontend/learnway/src/component/page/MainPage/SelectLanguage.jsx
+++ frontend/learnway/src/component/page/MainPage/SelectLanguage.jsx
@@ -2,16 +2,41 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
+import flagData from "./img/flagList.json";
 
-const LangSelect = styled.select`
-    width: 60%;
-    height: 40%;
+const LangSelect = styled.div`
+    // width: 60%;
+    // height: 40%;
+    text-align : center;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
 `;
 
-function SelectLanguage() {
+const Language = styled.div`
+    width :15%;
+    // height: 100%;
+    padding : 2vw 4vw;
+    float:left;
+    text-align: center;
+    margin: 0 auto;
+
+    &:hover {
+        background-color: rgb(0, 0, 0, 0.5);
+        color: rgb(255, 255, 255, 100);
+    }
+`;
+
+const LangImg = styled.img`
+    width: 4vw;
+    height: 2vw;
+`;
+
+const SelectLanguage = (props) => {
     const [optionList, setOptionList] = useState([]);
     const [value, setValue] = useState("");
     const dispatch = useDispatch();
+    
 
     // const TokenReducer = useSelector((state) => state.TokenReducer);
 
@@ -23,14 +48,19 @@ function SelectLanguage() {
             )
             .then(function (res) {
                 const data = res.data.language;
-                // console.log(data);
+                const flag = flagData.data; //국기 이미지 json으로 한번에
 
                 const options = [];
                 for (let i = 0; i < data.length; i++) {
                     options.push(
-                        <option key={data[i].languageId} value={data[i].name}>
-                            {data[i].code}
-                        </option>
+                        <Language key={data[i].languageId} value={data[i].name}
+                            onClick={(e) => {
+                            updateSelectedValue(data[i].name);
+                        }}
+                        >
+                            <LangImg src={flag[i].url}></LangImg><br></br>
+                            {data[i].name}
+                        </Language>
                     );
                 }
                 setOptionList(options);
@@ -42,10 +72,11 @@ function SelectLanguage() {
 
     //드롭다운 박스에서 선택한 요소로 redux 상태 업데이트하는 함수
     function updateSelectedValue(val) {
-        //드롭다운 박스 state업데이트
         setValue(val);
+        // console.log(val);
         //redux state 업데이트
         dispatch({ type: "matchLangUpdate", payload: val });
+
     }
 
     useEffect(() => {
@@ -53,15 +84,7 @@ function SelectLanguage() {
     }, []);
 
     return (
-        <LangSelect
-            onChange={(e) => {
-                updateSelectedValue(e.currentTarget.value);
-            }}
-            value={value}
-        >
-            <option value="" hidden>
-                Choose
-            </option>
+        <LangSelect>
             {optionList}
         </LangSelect>
     );
