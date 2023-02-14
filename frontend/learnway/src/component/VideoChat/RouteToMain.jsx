0c4cc01d97cfd2d224d8f2cc42939591e736fc60
jisoo <dlwltn0350@naver.com>
2023-02-14 13:31:08 +0900

    📝 UPDATE : #56 CSS 다듬기

--- frontend/learnway/src/component/VideoChat/RouteToMain.jsx
+++ frontend/learnway/src/component/VideoChat/RouteToMain.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import SessionEnd from './SessionEnd'
 //메인페이지로 이동할때까지 머무를 시간 설정
-const delayTime = 3000;
+const delayTime = 4000;
 
 function RouteToMain() {
     const [isTimeout, setIsTimeout] = useState(false);
@@ -18,10 +18,7 @@ function RouteToMain() {
             (
                 navigate("/", { replace: true })
                 ):(
-
                     <SessionEnd></SessionEnd>
-            
-            
             )
             }
         </>
