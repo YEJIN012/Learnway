ee81b1f812fa1fe9a0c18b1ad94031b5bf8d5e8d
HnM97 <chohm00790083@gmail.com>
2023-02-16 00:40:54 +0900

    🛠 FIX : #56 중복 코드 제거

--- frontend/learnway/src/component/VideoChat/Translate/Translate.jsx
+++ frontend/learnway/src/component/VideoChat/Translate/Translate.jsx
@@ -9,146 +9,129 @@ import MenuItem from "@mui/material/MenuItem";
 import Select from "@mui/material/Select";
 import FormControl from "@mui/material/FormControl";
 import { useSelector } from "react-redux";
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
 import { width } from "@mui/system";
 axios.defaults.headers["Access-Control-Allow-Credentials"] = true;
 axios.defaults.headers["Access-Control-Allow-Origin"] = "*";
 axios.defaults.withCredentials = true;
 
 const Frame = styled.div`
-    width: 100%;
-    height: 100%;
-    display: grid;
-    grid-template-rows: 0.3fr 3fr 0.3fr 3fr;
-    padding-left: 5%;
-    padding-right: 5%;
+  width: 100%;
+  height: 100%;
+  display: grid;
+  grid-template-rows: 0.3fr 3fr 0.3fr 3fr;
+  padding-left: 5%;
+  padding-right: 5%;
 `;
 const InnerFrame = styled.div`
-    // border: 1px solid;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
+  // border: 1px solid;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
 `;
 
 const Form = styled.form`
-    height: 80%;
+  height: 80%;
 `;
 const TextFrame = styled.textarea`
-    width: 100%;
-    height: 100%;
-    border-radius: 5px;
-    border: 1px solid #ccc;
-    resize: none;
-    font-size: 18px;
+  width: 100%;
+  height: 100%;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+  resize: none;
+  font-size: 18px;
 `;
 
 const TranslateArea = styled.textarea`
-    height: 80%;
-    width: 100%;
-    border-radius: 5px;
-    resize: none;
-    font-size: 18px;
+  height: 80%;
+  width: 100%;
+  border-radius: 5px;
+  resize: none;
+  font-size: 18px;
 `;
 
 const ButtonFrame = styled.div`
   display: flex;
   flex-direction: row-reverse;
-`
-function Translate() {
-  const myInfo = useSelector((state) => state.AuthReducer)
-  console.log(myInfo)
-  const oppoInfo = useSelector((state) => state.OpponentReducer)
-  console.log(oppoInfo)
-  const [value, setValue] = useState(""); // 검색 내용
-  const [translatedContent, setTranslatedContent] = useState(""); // 번역 내용
-  const [seletlang, setSelectlang] = useState("")  
-  const [resultLang, setResultLang] = useState("")
-  const { t } = useTranslation();
-
-const ResultLangFrame = styled.div`
-    color: #91a8d0;
-    font-size: large;
-    margin-bottom: 8px;
-    margin-left: 9px;
-    font-weight: 600;
 `;
 function Translate() {
-    const myInfo = useSelector((state) => state.AuthReducer);
-    console.log(myInfo);
-    const oppoInfo = useSelector((state) => state.OpponentReducer);
-    console.log(oppoInfo);
-    const [value, setValue] = useState(""); // 검색 내용
-    const [translatedContent, setTranslatedContent] = useState(""); // 번역 내용
-    const [seletlang, setSelectlang] = useState("");
-    const [resultLang, setResultLang] = useState("");
+  const myInfo = useSelector((state) => state.AuthReducer);
+  console.log(myInfo);
+  const oppoInfo = useSelector((state) => state.OpponentReducer);
+  console.log(oppoInfo);
+  const [value, setValue] = useState(""); // 검색 내용
+  const [translatedContent, setTranslatedContent] = useState(""); // 번역 내용
+  const [seletlang, setSelectlang] = useState("");
+  const [resultLang, setResultLang] = useState("");
+  const { t } = useTranslation();
 
-    useEffect(() => {
-        if (seletlang) {
-            setResultLang(
-                seletlang === myInfo.language.code
-                    ? oppoInfo.language.name
-                    : myInfo.language.name
-            );
-        }
-    }, [seletlang]);
-
-    async function getTranslate(props) {
-        console.log(props);
-        try {
-            const response = await axios.post(
-                "/papagoapi/v1/papago/n2mt",
-                {
-                    ////// user정보를 바탕으로 언어코드 변경 필요
-                    source:
-                        seletlang === myInfo.language.code
-                            ? myInfo.language.code
-                            : oppoInfo.language.code,
-                    target:
-                        seletlang === myInfo.language.code
-                            ? oppoInfo.language.code
-                            : myInfo.language.code,
-                    text: props,
-                },
-                {
-                    headers: {
-                        "X-Naver-Client-Id": `${process.env.REACT_APP_NAVER_ID}`,
-                        "X-Naver-Client-Secret": `${process.env.REACT_APP_NAVER_SECRET}`,
-                        // "Access-Control-Allow-Origin": "*",
-                    },
-                }
-            );
-            // handle success
-            console.log(response.data.message.result.translatedText);
-            setTranslatedContent(response.data.message.result.translatedText);
-            console.log("getTranslate");
-        } catch (error) {
-            // handle error
-            console.log(error);
-        }
+  useEffect(() => {
+    if (seletlang) {
+      setResultLang(
+        seletlang === myInfo.language.code
+          ? oppoInfo.language.name
+          : myInfo.language.name
+      );
     }
+  }, [seletlang]);
 
-    const handleTranslate = (e) => {
-        e.preventDefault();
-        console.log(value);
-        getTranslate(value);
-        console.log("handler");
-    };
+  async function getTranslate(props) {
+    console.log(props);
+    try {
+      const response = await axios.post(
+        "/papagoapi/v1/papago/n2mt",
+        {
+          ////// user정보를 바탕으로 언어코드 변경 필요
+          source:
+            seletlang === myInfo.language.code
+              ? myInfo.language.code
+              : oppoInfo.language.code,
+          target:
+            seletlang === myInfo.language.code
+              ? oppoInfo.language.code
+              : myInfo.language.code,
+          text: props,
+        },
+        {
+          headers: {
+            "X-Naver-Client-Id": `${process.env.REACT_APP_NAVER_ID}`,
+            "X-Naver-Client-Secret": `${process.env.REACT_APP_NAVER_SECRET}`,
+            // "Access-Control-Allow-Origin": "*",
+          },
+        }
+      );
+      // handle success
+      console.log(response.data.message.result.translatedText);
+      setTranslatedContent(response.data.message.result.translatedText);
+      console.log("getTranslate");
+    } catch (error) {
+      // handle error
+      console.log(error);
+    }
+  }
 
-    const handleChangeSelect = (e) => {
-        setSelectlang(e.target.value);
-    };
+  const handleTranslate = (e) => {
+    e.preventDefault();
+    console.log(value);
+    getTranslate(value);
+    console.log("handler");
+  };
 
-    const handleChangeText = (e) => {
-        setValue(e.target.value);
-    };
+  const handleChangeSelect = (e) => {
+    setSelectlang(e.target.value);
+  };
+
+  const handleChangeText = (e) => {
+    setValue(e.target.value);
+  };
 
   return (
     <CommonFrame
-      header = {<Title title={t('Translate')}></Title>}
+      header={<Title title={t("Translate")}></Title>}
       body={
         <Frame>
           <InnerFrame>
-          {/* <InputLabel id="demo-simple-select-label">Age</InputLabel>
+            {/* <InputLabel id="demo-simple-select-label">Age</InputLabel>
         <Select
           labelId="demo-simple-select-label"
           id="demo-simple-select"
@@ -160,46 +143,61 @@ function Translate() {
           <MenuItem value={20}>Twenty</MenuItem>
           <MenuItem value={30}>Thirty</MenuItem>
         </Select> */}
-        <FormControl sx={{margin:"10px 2px 6px 0px", width:"30%"}}>
-
-          <InputLabel id="demo-simple-select-label">{t('language')}</InputLabel>
-          <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select-label"
-          value={seletlang}
-          onChange={handleChangeSelect}
-          displayEmpty
-          label="Language"
-          sx={{color:"#91a8d0"}}
-          // inputProps={{ 'aria-label': 'Without label' }}
-          > 
-          <MenuItem value={myInfo.language.code}>{myInfo.language.name}</MenuItem>
-          <MenuItem value={oppoInfo.language.code}>{oppoInfo.language.name}</MenuItem>
-        </Select>
-          </FormControl>
+            <FormControl sx={{ margin: "10px 2px 6px 0px", width: "30%" }}>
+              <InputLabel id="demo-simple-select-label">
+                {t("language")}
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select-label"
+                value={seletlang}
+                onChange={handleChangeSelect}
+                displayEmpty
+                label="Language"
+                sx={{ color: "#91a8d0" }}
+                // inputProps={{ 'aria-label': 'Without label' }}
+              >
+                <MenuItem value={myInfo.language.code}>
+                  {myInfo.language.name}
+                </MenuItem>
+                <MenuItem value={oppoInfo.language.code}>
+                  {oppoInfo.language.name}
+                </MenuItem>
+              </Select>
+            </FormControl>
             {/* <h3>{"한국어"}</h3> */}
           </InnerFrame>
           <InnerFrame>
             <Form onSubmit={handleTranslate}>
               <TextFrame
-                  value={value}
-                  onChange={handleChangeText}
-                  placeholder={t('Enter the content to translate.')}
-                  // onKeyPress={onCheckEnter}
+                value={value}
+                onChange={handleChangeText}
+                placeholder={t("Enter the content to translate.")}
+                // onKeyPress={onCheckEnter}
               ></TextFrame>
               <ButtonFrame>
-                <Button type="submit" margin={"10px 0px"} id ="4" radius={"5px"} width={"inherit"} height={"3vw"} fontSize={"1.5vw"} textValue="translate"></Button>
+                <Button
+                  type="submit"
+                  margin={"10px 0px"}
+                  id="4"
+                  radius={"5px"}
+                  width={"inherit"}
+                  height={"3vw"}
+                  fontSize={"1.5vw"}
+                  textValue="translate"
+                ></Button>
               </ButtonFrame>
             </Form>
-          </InnerFrame >
+          </InnerFrame>
           <InnerFrame>
             <h3>{resultLang}</h3>
           </InnerFrame>
           <InnerFrame>
-              <TranslateArea disabled>{translatedContent}</TranslateArea>
+            <TranslateArea disabled>{translatedContent}</TranslateArea>
           </InnerFrame>
-      </Frame>  
-    }/>
+        </Frame>
+      }
+    />
   );
 }
 
--- frontend/learnway/src/component/page/Loading/RandomMatch.jsx
+++ frontend/learnway/src/component/page/Loading/RandomMatch.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
 import { useSelector } from "react-redux";
 import axios from "axios";
 import "./RandomMatch.css";
@@ -9,72 +9,71 @@ import ProfileImg from "../../ui/ProfileImg";
 import { useNavigate, useParams } from "react-router-dom";
 
 const Lang = styled.div`
-    width: 30%;
-    height: 10vh;
-    font-size: 3.5vh;
-    font-weight: 600;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-transform: uppercase;
+  width: 30%;
+  height: 10vh;
+  font-size: 3.5vh;
+  font-weight: 600;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-transform: uppercase;
 `;
 
 const Text = styled.div`
-    display: block;
-    font-size: 1em;
-    margin-inline-start: 0px;
-    margin-inline-end: 0px;
-    font-weight: 600;
+  display: block;
+  font-size: 1em;
+  margin-inline-start: 0px;
+  margin-inline-end: 0px;
+  font-weight: 600;
 `;
 
 const Interest = styled.div`
-    height: 2em;
-    width: inherit;
-    display: flex;
-    align-items: center;
-    margin-top: 8px;
-    margin-left: -8px;
-    //padding-top: 0.5em;
-    //padding-bottom: 0.5em;
-    padding-left: 0.5em;
-    padding-right: 0.5em;
-    border-radius: 20px;
-    background-color: #f0f0f0;
-    font-weight: bold;
-    font-size: 1.05em;
+  height: 2em;
+  width: inherit;
+  display: flex;
+  align-items: center;
+  margin-top: 8px;
+  margin-left: -8px;
+  //padding-top: 0.5em;
+  //padding-bottom: 0.5em;
+  padding-left: 0.5em;
+  padding-right: 0.5em;
+  border-radius: 20px;
+  background-color: #f0f0f0;
+  font-weight: bold;
+  font-size: 1.05em;
 `;
 
 function RandomMatch(props) {
-    const myInfo = useSelector((state) => state.AuthReducer);
-    const oppoInfo = useSelector((state) => state.OpponentReducer);
-    // const { oppoId, sessionId } = props;
-    //const oppoId = "learnwaytest@gmail.com";
-    const { roomId, recorder } = useParams();
+  const myInfo = useSelector((state) => state.AuthReducer);
+  const oppoInfo = useSelector((state) => state.OpponentReducer);
+  // const { oppoId, sessionId } = props;
+  //const oppoId = "learnwaytest@gmail.com";
+  const { roomId, recorder } = useParams();
 
-    const [friendCnt, setFriendCnt] = useState("");
-    //const [oppoInfo, setOppoInfo] = useState(opusr);
-    //setOppoInfo(opusr)
-    const delayTime = 7000;
-    const [isTimeout, setIsTimeout] = useState(false);
-    const navigate = useNavigate();
-    const { t } = useTranslation();
+  const [friendCnt, setFriendCnt] = useState("");
+  //const [oppoInfo, setOppoInfo] = useState(opusr);
+  //setOppoInfo(opusr)
+  const delayTime = 7000;
+  const [isTimeout, setIsTimeout] = useState(false);
+  const navigate = useNavigate();
+  const { t } = useTranslation();
 
-    console.log(oppoInfo, roomId)
-    
+  console.log(oppoInfo, roomId);
 
-    async function getFriendCnt() {
-        try {
-            const res = await axios.get("/api/friend/count", {
-                params: { userEmail: oppoInfo.userEmail },
-            });
-            console.log(res);
-            setFriendCnt(res.data.friendCnt);
-        } catch (error) {
-            console.log(error);
-        }
+  async function getFriendCnt() {
+    try {
+      const res = await axios.get("/api/friend/count", {
+        params: { userEmail: oppoInfo.userEmail },
+      });
+      console.log(res);
+      setFriendCnt(res.data.friendCnt);
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    /*
+  /*
         async function getUserInfo() {
             try {
                 const res = await axios
@@ -101,118 +100,114 @@ function RandomMatch(props) {
         }
     }
 */
-    function interestRernderer(array) {
-        let result = "";
-        if (array) {
-            for (let i = 0; i < array.length; i++) {
-                result += "#" + array[i].field + "  ";
-            }
-        }
-        return result;
+  function interestRernderer(array) {
+    let result = "";
+    if (array) {
+      for (let i = 0; i < array.length; i++) {
+        result += "#" + array[i].field + "  ";
+      }
     }
+    return result;
+  }
 
-    function redirect() {
-        if (isTimeout === true) {
-            navigate(
-                `/openvidu/${roomId}/${myInfo.userEmail}/${oppoInfo.userEmail}/${recorder}/${oppoInfo.language.languageId}/${myInfo.language.languageId}`,
-                { replace: true }
-            );
-            window.location.reload();
-        }
+  function redirect() {
+    if (isTimeout === true) {
+      navigate(
+        `/openvidu/${roomId}/${myInfo.userEmail}/${oppoInfo.userEmail}/${recorder}/${oppoInfo.language.languageId}/${myInfo.language.languageId}`,
+        { replace: true }
+      );
+      window.location.reload();
     }
+  }
 
-    useEffect(() => {
-        let timer = setTimeout(async () => {
-            setIsTimeout(true);
-        }, delayTime);
-        //getUserInfo();
-        getFriendCnt();
-    }, []);
-    console.log(isTimeout);
-    return (
-        <>
-            {isTimeout === true ? (
-                redirect()
-            ) : (
-                <div className="frame">
-                    <div className="framebody">
-                        <main className="ticket-system">
-                            <div className="top">
-                                <h1 className="title">
-                                    {t('😊 We succeeded in random matching based on your interest')}
-                                    <br />
-                                    {t('✈️ Please get ready to board now')}
-                                </h1>
-                                <div className="printer" />
-                            </div>
-                            <div className="receipts-wrapper">
-                                <div className="receipts">
-                                    <div className="receipt">
-                                        <img src={logo} alt="" width={"150"} />
+  useEffect(() => {
+    let timer = setTimeout(async () => {
+      setIsTimeout(true);
+    }, delayTime);
+    //getUserInfo();
+    getFriendCnt();
+  }, []);
+  console.log(isTimeout);
+  return (
+    <>
+      {isTimeout === true ? (
+        redirect()
+      ) : (
+        <div className="frame">
+          <div className="framebody">
+            <main className="ticket-system">
+              <div className="top">
+                <h1 className="title">
+                  {t(
+                    "😊 We succeeded in random matching based on your interest"
+                  )}
+                  <br />
+                  {t("✈️ Please get ready to board now")}
+                </h1>
+                <div className="printer" />
+              </div>
+              <div className="receipts-wrapper">
+                <div className="receipts">
+                  <div className="receipt">
+                    <img src={logo} alt="" width={"150"} />
 
-                                        <div className="route">
-                                            <Lang>{myInfo.language.name}</Lang>
-                                            <svg
-                                                className="plane-icon"
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                viewBox="0 0 510 510"
-                                            >
-                                                <path
-                                                    fill="#91a8d0"
-                                                    d="M497.25 357v-51l-204-127.5V38.25C293.25 17.85 275.4 0 255 0s-38.25 17.85-38.25 38.25V178.5L12.75 306v51l204-63.75V433.5l-51 38.25V510L255 484.5l89.25 25.5v-38.25l-51-38.25V293.25l204 63.75z"
-                                                />
-                                            </svg>
-                                            <Lang>
-                                                {oppoInfo.language.name}
-                                            </Lang>
-                                        </div>
-                                        <div className="details">
-                                            <div className="item">
-                                                <span>Name</span>
-                                                <h3>{oppoInfo.name}</h3>
-                                            </div>
-                                            <div className="item">
-                                                <span>Birth</span>
-                                                <h3>{oppoInfo.birthDay}</h3>
-                                            </div>
-                                            <div className="item">
-                                                <span>Email</span>
-                                                <h6>{oppoInfo.userEmail}</h6>
-                                            </div>
-                                            <div className="item">
-                                                <span>Friends</span>
-                                                <h3>{friendCnt}</h3>
-                                            </div>
-                                        </div>
-                                        <div className="details">
-                                            <div className="item">
-                                                <span>Bio</span>
-                                                <h3>{oppoInfo.bio}</h3>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="receipt qr-code">
-                                        <ProfileImg
-                                            src={oppoInfo.imgUrl}
-                                        ></ProfileImg>
-                                        <div className="description">
-                                            <Text>{t('Hi🙌')}</Text>
-                                            <Text>{t('Our interest is')}</Text>
-                                            <Interest>
-                                                {interestRernderer(
-                                                    oppoInfo.interests
-                                                )}
-                                            </Interest>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </main>
+                    <div className="route">
+                      <Lang>{myInfo.language.name}</Lang>
+                      <svg
+                        className="plane-icon"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 510 510"
+                      >
+                        <path
+                          fill="#91a8d0"
+                          d="M497.25 357v-51l-204-127.5V38.25C293.25 17.85 275.4 0 255 0s-38.25 17.85-38.25 38.25V178.5L12.75 306v51l204-63.75V433.5l-51 38.25V510L255 484.5l89.25 25.5v-38.25l-51-38.25V293.25l204 63.75z"
+                        />
+                      </svg>
+                      <Lang>{oppoInfo.language.name}</Lang>
                     </div>
-                
-            )}
-        </>
-    );
+                    <div className="details">
+                      <div className="item">
+                        <span>Name</span>
+                        <h3>{oppoInfo.name}</h3>
+                      </div>
+                      <div className="item">
+                        <span>Birth</span>
+                        <h3>{oppoInfo.birthDay}</h3>
+                      </div>
+                      <div className="item">
+                        <span>Email</span>
+                        <h6>{oppoInfo.userEmail}</h6>
+                      </div>
+                      <div className="item">
+                        <span>Friends</span>
+                        <h3>{friendCnt}</h3>
+                      </div>
+                    </div>
+                    <div className="details">
+                      <div className="item">
+                        <span>Bio</span>
+                        <h3>{oppoInfo.bio}</h3>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="receipt qr-code">
+                    <ProfileImg src={oppoInfo.imgUrl}></ProfileImg>
+                    <div className="description">
+                      <Text>{t("Hi🙌")}</Text>
+                      <Text>{t("Our interest is")}</Text>
+                      <Interest>
+                        {interestRernderer(oppoInfo.interests)}
+                      </Interest>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </main>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default RandomMatch;
